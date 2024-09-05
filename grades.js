9aff8cba66788649922cb973ae38bb504457ed8a
mahdi786islam <mahdiislam786@gmail.com>
2024-09-05 13:34:06 +0600

    grades

---
+++ grades.js
@@ -0,0 +1,17 @@
+const score = 60;
+
+if( score == 100 || score >= 90){
+    console.log('Your grade is A')
+}
+else if( score >= 80 || score == 89){
+    console.log('Your grade is B')
+}
+else if( score >= 70 || score == 79){
+    console.log('Your grade is C')
+}
+else if( score >= 60 || score == 69){
+    console.log('Your grade is D')
+}
+else{
+    console.log('No grades determined')
+}

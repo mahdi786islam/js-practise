f9e6baf6043b4d923a0d2265ba48e3660fe4c234
mahdi786islam <mahdiislam786@gmail.com>
2024-09-06 12:11:09 +0600

    push

--- function.js
+++ function.js
@@ -1,6 +1,20 @@
-function dobol(number){
-    const dobbol = number * 2;
-    console.log(dobbol);
+// function dobol(number){
+//     const dobbol = number * 2;
+//     // console.log(dobbol);
+//     return dobbol;
+// }
+
+// dobol(43);
+
+
+// const dabol = dobol(13);
+// console.log(dabol);
+
+function add(price1 , price2){
+    const total = price1 + price2;
+    return total;
 }
 
-dobol(43);
+
+const bill = add(5, 34);
+console.log(bill);

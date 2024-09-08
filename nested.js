9d6f43adbfba436f9a23554acbd7597e839dab07
mahdi786islam <mahdiislam786@gmail.com>
2024-09-08 16:02:52 +0600

    nested practised

---
+++ nested.js
@@ -0,0 +1,19 @@
+let myScore = 72
+let friendScore = 48
+if( myScore > 80 ){
+    if( friendScore > 80 ){
+        console.log( "Ajke dupure kacchi" )
+    }
+    else if ( friendScore > 60 || friendScore === 80  ){
+        console.log( "porer bar bhalo moton exam dis" )
+    }
+    else if ( friendScore > 40 || friendScore === 60 ){
+        console.log ( "tui beda faltu" )
+    }
+    else {
+        console.log( "tui matis na" )
+    }
+}
+else{
+    console.log( "biday prithibi" )
+}

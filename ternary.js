40c7611dedcbae2e3975d8846dc5cd717f17cef5
mahdi786islam <mahdiislam786@gmail.com>
2024-09-08 16:18:15 +0600

    ternary

---
+++ ternary.js
@@ -0,0 +1,13 @@
+let num1 = 5;
+let num2 = 4;
+let result = num1 * 2;
+let sum = num1 + num2;
+
+// if( num1 > num2 ){
+//     console.log(result);
+// }
+// else{
+//     console.log(sum);
+// }
+
+num1 > num2 ? console.log(result) : console.log(sum);
